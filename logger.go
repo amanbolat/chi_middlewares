2f6d3bd880173615d3d7c5ce1630fb87dc0f79e8
amanbolat <amanbolat@me.com>
2018-06-25 18:50:31 +0800

    added apex logger

--- logger.go
+++ logger.go
@@ -1,31 +1,24 @@
 package chi_middlewares
 
 import (
-	"fmt"
 	"net/http"
 	"runtime/debug"
 	"time"
-
 	"github.com/go-chi/chi/middleware"
 	"github.com/rs/zerolog"
+	alog "github.com/apex/log"
+	"github.com/apex/log/handlers/json"
+	"os"
 )
 
 func LoggerMiddleware(logger *zerolog.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
-			scheme := "http"
-			if r.TLS != nil {
-				scheme = "https"
-			}
-
 			log := logger.With().Logger()
 
 			if reqID := GetRequestID(r.Context()); reqID != "" {
 				log = log.With().Str("request_id", reqID).Logger()
 			}
-
-			// log start request
-			log.Info().Timestamp().Msg("start_request")
 
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
 
@@ -44,14 +37,62 @@ func LoggerMiddleware(logger *zerolog.Logger) func(next http.Handler) http.Handl
 					"remote_ip":  r.RemoteAddr,
 					"host":       r.Host,
 					"proto":      r.Proto,
-					"uri":        fmt.Sprintf("%s://%s%s", scheme, r.Host, r.URL.RequestURI()),
 					"method":     r.Method,
 					"user_agent": r.Header.Get("User-Agent"),
 					"status":     ww.Status(),
 					"latency_ms": float64(t2.Sub(t1).Nanoseconds()) / 1000000.0,
 					"bytes_in":   r.Header.Get("Content-Length"),
 					"bytes_out":  ww.BytesWritten(),
-				}).Msg("end_request")
+				}).Msg("handled_request")
+			}()
+
+			next.ServeHTTP(ww, r)
+		}
+		return http.HandlerFunc(fn)
+	}
+}
+
+func ApexLoggerMiddleware() func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			log := &alog.Logger{
+				Handler: json.New(os.Stderr),
+				Level:   0,
+			}
+
+			e := alog.NewEntry(log)
+
+			if reqID := GetRequestID(r.Context()); reqID != "" {
+				e = e.WithField("request_id", reqID)
+			}
+
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+			t1 := time.Now()
+			defer func() {
+				t2 := time.Now()
+
+				// Recover and record stack traces in case of a panic
+				if rec := recover(); rec != nil {
+					e.WithFields(alog.Fields{
+						"recover_info": rec,
+						"debug_stack": debug.Stack(),
+					}).Error("error_request")
+					http.Error(ww, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				}
+
+				// log end request
+				e.WithFields(alog.Fields{
+					"remote_ip":  r.RemoteAddr,
+					"host":       r.Host,
+					"proto":      r.Proto,
+					"method":     r.Method,
+					"user_agent": r.Header.Get("User-Agent"),
+					"status":     ww.Status(),
+					"latency_ms": float64(t2.Sub(t1).Nanoseconds()) / 1000000.0,
+					"bytes_in":   r.Header.Get("Content-Length"),
+					"bytes_out":  ww.BytesWritten(),
+				}).Info("handled_request")
 			}()
 
 			next.ServeHTTP(ww, r)
